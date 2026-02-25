063f47f39363a2f1d00cd6caf61c751d726a7cbc
kadreykohler-bit <seu-email-da-conta-kadreykohler-bit@exemplo.com>
2026-02-24 21:20:05 -0300

    Adicionando o desafio 03 na pasta formacao

---
+++ Formacao-Logica-Programacao-Dio-/03-jornada-do-heroi/index.js
@@ -0,0 +1,9 @@
+// Captura as entradas
+const posicaoInicial = parseInt(gets());
+const totalPassos = parseInt(gets());
+
+// Realiza o cálculo lógico de deslocamento
+const posicaoFinal = posicaoInicial + totalPassos;
+
+// Exibe o resultado formatado
+print("Posicao final do heroi:", posicaoFinal);
